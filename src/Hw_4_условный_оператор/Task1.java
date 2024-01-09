b2944d23269be11ebf9966e5d241ca3854b98b11
Keenan88 <rembosaratov@gmail.com>
2024-01-09 20:16:28 +0500

    Task1

--- src/Hw_4_условный_оператор/Task1.java
+++ src/Hw_4_условный_оператор/Task1.java
@@ -3,7 +3,12 @@ package Hw_4_условный_оператор;
 public class Task1 {
 
     public static void main(String[] args) {
-
+        int age = 22;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " ,то он совершеннолетний" );
+        } else {
+            System.out.println("Если возраст человека равен " + age + " ,то он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
 
 }
