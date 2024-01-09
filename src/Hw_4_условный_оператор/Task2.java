635b48235f97f429a88225ee7813612f5315ff15
Keenan88 <rembosaratov@gmail.com>
2024-01-09 21:01:56 +0500

    Task1

---
+++ src/Hw_4_условный_оператор/Task2.java
@@ -0,0 +1,18 @@
+package Hw_4_условный_оператор;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        int temperature = 15;
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        }
+        if (temperature > 5) {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        }
+    }
+}
+
+
+
+
