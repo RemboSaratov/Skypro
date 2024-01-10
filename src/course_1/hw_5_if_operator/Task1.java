14dd4cb7d96ed436963a16b9885adc0abad59b4b
Keenan88 <rembosaratov@gmail.com>
2024-01-10 13:55:59 +0500

    Task1

---
+++ src/course_1/hw_5_if_operator/Task1.java
@@ -0,0 +1,13 @@
+package course_1.hw_5_if_operator;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+}
