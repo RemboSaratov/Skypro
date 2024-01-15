0c5e60c7a39e3db462f4ee250935640276224fc5
Keenan88 <rembosaratov@gmail.com>
2024-01-15 20:22:35 +0500

    Task10

---
+++ src/course_1/hw_6_cycle/Task10.java
@@ -0,0 +1,11 @@
+package course_1.hw_6_cycle;
+
+public class Task10 {
+
+    public static void main(String[] args) {
+        int two = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(two + "*" + i + "=" + two * i);
+        }
+    }
+}
