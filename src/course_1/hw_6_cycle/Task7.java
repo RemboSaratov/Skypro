0710ac9fdef9c1b9d937ac8db00b0058068e4874
Keenan88 <rembosaratov@gmail.com>
2024-01-15 19:18:58 +0500

    Task7

---
+++ src/course_1/hw_6_cycle/Task7.java
@@ -0,0 +1,11 @@
+package course_1.hw_6_cycle;
+
+public class Task7 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+    }
+}
