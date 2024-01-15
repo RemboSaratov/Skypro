f3a9b23b836d7f8da3dd73b183c339b907b92895
Keenan88 <rembosaratov@gmail.com>
2024-01-15 19:07:34 +0500

    Task5

---
+++ src/course_1/hw_6_cycle/Task5.java
@@ -0,0 +1,11 @@
+package course_1.hw_6_cycle;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+    }
+}
