05169e18ef11731098fd88bc8ccf4cc0dd81da58
Keenan88 <rembosaratov@gmail.com>
2024-01-15 18:20:15 +0500

    Task4

---
+++ src/course_1/hw_6_cycle/Task4.java
@@ -0,0 +1,11 @@
+package course_1.hw_6_cycle;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+}
