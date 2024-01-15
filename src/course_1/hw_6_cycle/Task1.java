9cd94447a0997b7e1799d12dacb01afa11f6c4f9
Keenan88 <rembosaratov@gmail.com>
2024-01-15 17:55:37 +0500

    Task1

---
+++ src/course_1/hw_6_cycle/Task1.java
@@ -0,0 +1,11 @@
+package course_1.hw_6_cycle;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        for (i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
