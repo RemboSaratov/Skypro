c5de1e3b7573dd464f76d10dc3a63501c9a24301
Keenan88 <rembosaratov@gmail.com>
2024-01-15 19:57:22 +0500

    Task9

--- src/course_1/hw_6_cycle/Task9.java
+++ src/course_1/hw_6_cycle/Task9.java
@@ -1,15 +1,16 @@
 package course_1.hw_6_cycle;
 
-public class Task8 {
+public class Task9 {
 
     public static void main(String[] args) {
         int savings = 29000;
         int savingsForyear = 0;
         for (int i = 0; i < 12; i++) {
+            savingsForyear = savingsForyear + savingsForyear/100;
             savingsForyear = savingsForyear + savings;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + savingsForyear
-                   + " рублей");// отработка переноса строки
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + savingsForyear + " рублей");
         }
         System.out.println("Накопления за год составили " + savingsForyear + " рублей");
     }
 }
+
