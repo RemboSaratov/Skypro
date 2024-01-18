edfc87f8b4fe601a76aa839d0743d22ee4bcbc49
Keenan88 <rembosaratov@gmail.com>
2024-01-18 17:13:26 +0500

    1 2 correct

--- src/course_1/hw_6_cycle/Task2.java
+++ src/course_1/hw_6_cycle/Task2.java
@@ -3,8 +3,7 @@ package course_1.hw_6_cycle;
 public class Task2 {
 
     public static void main(String[] args) {
-        int a = 0;
-        for (a = 10; a > 0; a--) {
+        for (int a = 10; a > 0; a--) {
             System.out.println(a);
         }
     }
