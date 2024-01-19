a83cc855e15b6e16e36fdf405c7f7719c3ef59ab
Keenan88 <rembosaratov@gmail.com>
2024-01-19 12:33:55 +0500

    Task1

---
+++ src/course_1/hw_7_cycle/Task1.java
@@ -0,0 +1,15 @@
+package course_1.hw_7_cycle;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        int deposit = 15000;
+        int savings = 0;
+        int month = 0;
+        while (savings <= 2459000) {
+            savings = deposit + savings;
+            month = month + 1;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + savings + " рублей");
+        }
+    }
+}
