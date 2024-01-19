802c4ded1ecb16e0a72a2e45a01dfb63abf06825
Keenan88 <rembosaratov@gmail.com>
2024-01-19 12:37:10 +0500

    Task1 доработка с учетом процентов

--- src/course_1/hw_7_cycle/Task1.java
+++ src/course_1/hw_7_cycle/Task1.java
@@ -7,6 +7,7 @@ public class Task1 {
         int savings = 0;
         int month = 0;
         while (savings <= 2459000) {
+            savings = savings + savings/100;
             savings = deposit + savings;
             month = month + 1;
             System.out.println("Месяц " + month + ", сумма накоплений равна " + savings + " рублей");
