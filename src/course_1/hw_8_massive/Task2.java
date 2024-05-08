16b7a8e45f275aad9e8c5691cd0017a97fba1950
Keenan88 <rembosaratov@gmail.com>
2024-05-08 17:58:15 +0500

    Task1

--- src/course_1/hw_8_massive/Task2.java
+++ src/course_1/hw_8_massive/Task2.java
@@ -7,20 +7,23 @@ public class Task2 {
         number[0] = 1;
         number[1] = 2;
         number[2] = 3;
-        System.out.print(number[0] + ", ");
-        System.out.print(number[1] + ", ");
-        System.out.println(number[2]);
+        for (int i = 0; i < 3; i++){
+            System.out.print(number[i] + ", ");
+        }
+        System.out.println();
         //
         double[] weights = {1.57, 7.654, 9.986};
-        System.out.print(weights[0] + ", ");
-        System.out.print(weights[1] + ", ");
-        System.out.println(weights[2]);
+        for (int i = 0; i < 3; i++) {
+            System.out.print(weights[i] + ", ");
+        }
+        System.out.println();
         //
-        double [] box = new double[4];
+        double[] box = new double[4];
         box[0] = 1.1;
         box[1] = 5.98;
         box[2] = 9.8787;
         box[3] = 45.876978;
-        System.out.print(box[0] + ", " + box[1] + ", " + box[2] + ", " + box[3]);
+        for (double element : box)
+        System.out.print(element + ", ");
     }
 }
--- src/course_1/hw_8_massive/Task3.java
+++ src/course_1/hw_8_massive/Task3.java
@@ -7,22 +7,25 @@ public class Task3 {
         number[0] = 1;
         number[1] = 2;
         number[2] = 3;
-        System.out.print(number[2] + ", ");
-        System.out.print(number[1] + ", ");
-        System.out.println(number[0]);
+        for (int i = number.length - 1; i >= 0; i--) {
+            System.out.print(number[i] + ", ");
+        }
+        System.out.println();
+        //
         double[] weights = {1.57, 7.654, 9.986};
-        System.out.print(weights[2] + ", ");
-        System.out.print(weights[1] + ", ");
-        System.out.println(weights[0]);
-        double [] box = new double[4];
+        for (int i = weights.length - 1; i >= 0; i--) {
+            System.out.print(weights[i] + ", ");
+        }
+        System.out.println();
+        //
+        double[] box = new double[4];
         box[0] = 1.1;
         box[1] = 5.98;
         box[2] = 9.8787;
         box[3] = 45.876978;
-        System.out.print(box[3] + ", ");
-        System.out.print(box[2] + ", ");
-        System.out.print(box[1] + ", ");
-        System.out.print(box[0]);
+        for (int i = box.length - 1; i >= 0; i--) {
+            System.out.print(box[i] + ", ");
+        }
     }
-
 }
+
