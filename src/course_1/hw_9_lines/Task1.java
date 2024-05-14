25ebb0e8ced9529979c41aca3baa0fbba49ce871
Keenan88 <rembosaratov@gmail.com>
2024-05-14 11:51:16 +0500

    Hw_Lines_Task1,2,3

--- src/course_1/hw_9_lines/Task1.java
+++ src/course_1/hw_9_lines/Task1.java
@@ -3,10 +3,10 @@ package course_1.hw_9_lines;
 public class Task1 {
 
     public static void main(String[] args) {
-        String firstName = "Ivan ";
+        String firstName = "Ivan";
         String middleName = "Ivanovich";
-        String lastName = "Ivanov ";
-        String fullName = lastName + firstName + middleName;
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Ф.И.О. Сотрудника - " + fullName);
     }
 
