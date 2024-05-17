a79f5816efe89c9628fa91c084ddb5aa62fe4faf
Keenan88 <rembosaratov@gmail.com>
2024-05-17 12:44:30 +0500

    Hw_Lines_Task1,2,3

--- src/course_1/hw_9_method/Task3.java
+++ src/course_1/hw_9_method/Task3.java
@@ -3,9 +3,9 @@ package course_1.hw_9_method;
 public class Task3 {
 
     public static void main(String[] args) {
-        cardDelivery(95);
+        printDeliveryDateCard(95);
     }
-    public static void cardDelivery(int distance) {
+    public static void printDeliveryDateCard(int distance) {
        if (distance > 0 && distance <= 20) {
            System.out.println("Потребуется 1 день доставки");
        } else if (distance > 20 && distance <= 60) {
