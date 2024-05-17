92fbb4aa7c3618d3dc0658e003a4c67fe48620a2
Keenan88 <rembosaratov@gmail.com>
2024-05-17 12:27:58 +0500

    Hw_Lines_Task1,2,3

--- src/course_1/hw_9_method/Task1.java
+++ src/course_1/hw_9_method/Task1.java
@@ -3,10 +3,10 @@ package course_1.hw_9_method;
 public class Task1 {
 
     public static void main(String[] args) {
-        int year = 2024; // or receivingYear(2024)
-        receivingYear(year);
+        int year = 2024; // or printLeapYearStatus(2024)
+        printLeapYearStatus(year);
     }
-    public static void receivingYear(int year) {
+    public static void printLeapYearStatus(int year) {
         if ((year > 1584 && year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println(year + " год является високосным");
         } else {
--- src/course_1/hw_9_method/Task2.java
+++ src/course_1/hw_9_method/Task2.java
@@ -10,12 +10,14 @@ public class Task2 {
     public static void checkDevice(int clientOs, int clientDeviceYear) {
         if (clientDeviceYear < 2015 && clientOs == 1) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientDeviceYear >= 2015 && clientOs == 1) {
+        } else if (clientDeviceYear >= 2015 && clientOs == 1) { // старше текущего года
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientDeviceYear < 2015 && clientOs != 1) {
+        } else if (clientDeviceYear < 2015 && clientOs == 0) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientDeviceYear >= 2015 && clientOs != 1) {
+        } else if (clientDeviceYear >= 2015 && clientOs == 0) { // старше текущего года
             System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Ваша операционная система не поддерживается"); // если != 0 && != 1
         }
     }
 }
