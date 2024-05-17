cfd484a17b5fd1e4d5562884b6bad1ffe05017e3
Keenan88 <rembosaratov@gmail.com>
2024-05-17 12:41:51 +0500

    Hw_Lines_Task1,2,3

--- src/course_1/hw_9_method/Task2.java
+++ src/course_1/hw_9_method/Task2.java
@@ -9,13 +9,14 @@ public class Task2 {
     }
     public static void checkDevice(int clientOs, int clientDeviceYear) {
         if (clientDeviceYear < 2015 && clientOs == 1) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientDeviceYear >= 2015 && clientOs == 1) { // старше текущего года
             System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOs == 1) { // старше текущего года
+            System.out.println("Установите облегченную версию для Android по ссылке");
+            // в оригинальной задаче устройству старше 2015 нужно было ставить обычную версию / исправил
         } else if (clientDeviceYear < 2015 && clientOs == 0) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientDeviceYear >= 2015 && clientOs == 0) { // старше текущего года
-            System.out.println("Установите версию приложения для iOS по ссылке");
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else {
             System.out.println("Ваша операционная система не поддерживается"); // если != 0 && != 1
         }
