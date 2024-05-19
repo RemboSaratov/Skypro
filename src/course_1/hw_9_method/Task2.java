1af194a1e946567fe11eeaed0b07f67ae41d1b1e
Keenan88 <rembosaratov@gmail.com>
2024-05-19 09:59:39 +0500

    Hw_Lines_Task1,2,3

--- src/course_1/hw_9_method/Task2.java
+++ src/course_1/hw_9_method/Task2.java
@@ -1,24 +1,28 @@
 package course_1.hw_9_method;
 
+import java.time.LocalDate;
+
 public class Task2 {
 
     public static void main(String[] args) {
-        int clientOs = 1; //
+        int clientOs = 0;
         int clientDeviceYear = 2024;
-        checkDevice(clientOs, clientDeviceYear); // or checkDevice(1,2024)
+        checkDevice(clientOs, clientDeviceYear); // or checkDevice(0,2023)
     }
+
     public static void checkDevice(int clientOs, int clientDeviceYear) {
-        if (clientDeviceYear < 2015 && clientOs == 1) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientDeviceYear < currentYear && clientOs == 1) {
+            // год выпуска устройства меньше текущего года,значит оно "старше"(из условия задачи) текущего года, я так понимаю
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientDeviceYear == currentYear && clientOs == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientDeviceYear >= 2015 && clientOs == 1) { // старше текущего года
-            System.out.println("Установите облегченную версию для Android по ссылке");
-            // в оригинальной задаче устройству старше 2015 нужно было ставить обычную версию / исправил
-        } else if (clientDeviceYear < 2015 && clientOs == 0) {
+        } else if (clientDeviceYear < currentYear && clientOs == 0) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientDeviceYear >= 2015 && clientOs == 0) { // старше текущего года
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear == currentYear && clientOs == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
-            System.out.println("Ваша операционная система не поддерживается"); // если != 0 && != 1
+            System.out.println("Не правильно введенные данные");
         }
     }
 }
