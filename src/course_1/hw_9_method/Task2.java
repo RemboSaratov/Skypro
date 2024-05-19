68ce0730b79f0619c2539ac6d2e04382d191c7e9
Keenan88 <rembosaratov@gmail.com>
2024-05-19 17:29:56 +0500

    Hw_Lines_Task1,2,3

--- src/course_1/hw_9_method/Task2.java
+++ src/course_1/hw_9_method/Task2.java
@@ -12,7 +12,7 @@ public class Task2 {
             System.out.println("Не правильно введенные данные");
         } else {
             int currentYear = LocalDate.now().getYear();
-            var type = clientOs == 0 ? "Android" : "iOs";
+            var type = clientOs == 1 ? "Android" : "iOs";
             var deviceYear = clientDeviceYear < currentYear ? "облегченную" : "обычную";
             System.out.println("Установите " + deviceYear + " версию для системы " + type);
         }
